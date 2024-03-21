1995a0495f3033d406e24a6dd0cae6e7413989a5
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-21 22:20:06 +0000

    Added bash installer script

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 mkdir -p $HOME/.multi-tasker/bin
-curl bin_url -o $HOME/.multi-tasker/bin/mlt
-echo "export PATH=\"$HOME/.multi-tasker/bin\"" >> $HOME/.profile
+curl https://afreechameleon.github.io/files/mlt.tar.gz -o $PWD/mlt.tar.gz
+tar xvfz $PWD/mlt.tar.gz
+mv $PWD/mlt $HOME/.multi-tasker/bin
+rm $PWD/mlt.tar.gz
+for f in $HOME/.*rc
+do
+    if ! grep -q "export PATH=\"\$PATH:$HOME/.multi-tasker/bin\"" $f
+    then
+        echo "export PATH=\"\$PATH:$HOME/.multi-tasker/bin\"" >> $f
+    fi
+done
+
+echo "To use mlt in this session, run: export PATH=\"\$PATH:$HOME/.multi-tasker/bin\""
