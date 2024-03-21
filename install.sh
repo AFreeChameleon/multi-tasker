f3b30beb18a9e300fc39ef181082611c9b56ff3d
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-21 22:30:48 +0000

    Added silent mode to curl

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p $HOME/.multi-tasker/bin
-curl https://afreechameleon.github.io/files/mlt.tar.gz -o $PWD/mlt.tar.gz
+curl https://afreechameleon.github.io/files/mlt.tar.gz -s -o $PWD/mlt.tar.gz
 tar xvfz $PWD/mlt.tar.gz
 mv $PWD/mlt $HOME/.multi-tasker/bin
 rm $PWD/mlt.tar.gz
