3a784a3a1694892cdb319f7819f124fe4eb04df8
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-26 01:41:48 +0000

    Added out null

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
-curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip
-New-Item "$env:USERPROFILE\.multi-tasker\bin" -ItemType Directory -Force
+curl --silent https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip | Out-Null
+New-Item "$env:USERPROFILE\.multi-tasker\bin" -ItemType Directory -Force | Out-Null
 Expand-Archive -Force "$env:USERPROFILE\mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:USERPROFILE\.multi-tasker\bin", "User")
 $env:PATH += ";$env:USERPROFILE\.multi-tasker\bin"
