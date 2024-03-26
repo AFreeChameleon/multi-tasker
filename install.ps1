528df5cc5de9b86c43021ad17a5276deb4da85d8
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-26 01:44:04 +0000

    Silent just doesn't exist

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-curl --silent https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip | Out-Null
+curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip | Out-Null
 New-Item "$env:USERPROFILE\.multi-tasker\bin" -ItemType Directory -Force | Out-Null
 Expand-Archive -Force "$env:USERPROFILE\mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:USERPROFILE\.multi-tasker\bin", "User")
