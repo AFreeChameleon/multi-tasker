283d25fae42cb541bcad838e4bdb0effeda313f3
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-26 01:22:19 +0000

    Windows install script

---
+++ install.ps1
@@ -0,0 +1,6 @@
+curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip -s
+New-Item "$env:USERPROFILE/.multi-tasker/bin" -ItemType Directory -Force
+Expand-Archive -Force "mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:USERPROFILE\.multi-tasker\bin", "User")
+$env:PATH += ";$env:USERPROFILE\.multi-tasker\bin"
+echo "Install finished, run: `mlt help` to get started"
