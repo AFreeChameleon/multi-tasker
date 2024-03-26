7f07054bac35e8b6b84c39c1b17db67d0ba675a9
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-26 01:34:26 +0000

    Added remove item

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip -s
-New-Item "$env:USERPROFILE/.multi-tasker/bin" -ItemType Directory -Force
-Expand-Archive -Force "$env:USERPROFILE/mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
+New-Item "$env:USERPROFILE\.multi-tasker\bin" -ItemType Directory -Force
+Expand-Archive -Force "$env:USERPROFILE\mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:USERPROFILE\.multi-tasker\bin", "User")
 $env:PATH += ";$env:USERPROFILE\.multi-tasker\bin"
+Remove-Item "$env:USERPROFILE\mlt-win.zip"
 echo "Install finished, run: 'mlt help' to get started"
