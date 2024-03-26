5ea261a89cf193b915dc55167538880e2f1d0db1
AFreeChameleon <ben.evans@chameleo.dev>
2024-03-26 01:35:51 +0000

    Removed silent

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip -s
+curl https://afreechameleon.github.io/files/mlt-win.zip -o mlt-win.zip
 New-Item "$env:USERPROFILE\.multi-tasker\bin" -ItemType Directory -Force
 Expand-Archive -Force "$env:USERPROFILE\mlt-win.zip" "$env:USERPROFILE\.multi-tasker\bin"
 [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$env:USERPROFILE\.multi-tasker\bin", "User")
